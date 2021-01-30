8674e23926c614d229ba6a0d0dab79ddef336557
Piotrek <piotr.gorecki01@student.wat.edu.pl>
2021-01-30 23:26:52 +0100

    for Azure

--- DjangoPracaInzynierska/settings.py
+++ DjangoPracaInzynierska/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os, psycopg2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.contrib import staticfiles
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -127,6 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = 'staticfiles'
 
 STATIC_URL = '/static/'
 
