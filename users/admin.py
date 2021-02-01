6cb00fe5adb6097b280180e285fc716a09cc05ea
Piotrek <piotr.gorecki01@student.wat.edu.pl>
2021-02-01 17:40:13 +0100

    poprawki dodawania projektu

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Profile, Account
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
 
 # Register your models here.
+
 
 admin.site.register(Profile)
 admin.site.register(Account)
